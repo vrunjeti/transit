1bab2ad8da7736b787851f8258e5136d9affdb32
Varun Munjeti <vrunjeti@gmail.com>
2016-04-11 04:41:25 -0500

    created dist

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {render} from 'react-dom'
 import App from './App'
-import './styles/styles.scss' //Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
+import './styles/styles.scss'
 
 render(
   <App />,
